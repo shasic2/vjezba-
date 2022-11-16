6e00c102969c6019c5fdc3c37d05ec968e0d351f
shasic2 <shasic2@etf.unsa.ba>
2022-11-16 19:07:22 +0100

    prvi zadatak

--- src/main/java/ba/unsa/etf/rpr/App.java
+++ src/main/java/ba/unsa/etf/rpr/App.java
@@ -1,13 +1,24 @@
 package ba.unsa.etf.rpr;
 
+import java.util.Scanner;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Unesite prvi broj: ");
+        int first = scanner.nextInt();
+        System.out.println("Unesite drugi broj: ");
+        int second = scanner.nextInt();
+
+
+        System.out.println( "Suma brojeva je: " +(first+second) );
+
     }
+
+
 }
